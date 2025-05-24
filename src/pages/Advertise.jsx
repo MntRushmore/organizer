bbb7d958c1b1e9b00afdb59f8dc35fb145c8547d
Saahil <neon@saahild.com>
2025-05-23 21:05:38 -0400

    update: a

---
+++ src/pages/Advertise.jsx
@@ -0,0 +1,29 @@
+export default function app({ nextPage, backPage,currentPage }) {
+    return <div className="hero bg-base-200 min-h-screen">
+    <div className="hero-content text-center">
+    <div className="max-w-xl">
+    <h1 className="text-5xl font-bold">3. Advertising YSWS!</h1>
+    <p className="py-1">
+Each ysws must have a decent amount of participents or whats the point in this funding 
+    </p>
+    <br />
+<h3>Slack channel #my-ysws</h3>
+<p>Locking down your channels for your ysws is very important as its a great way to check if the name is taken! for eg</p>
+<img src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/b645ddbff6602f360027a0df27b3e0ac1026647f_image.png" />
+<br />
+<p>If your name is free congrats! i would then recommend creating the following channels:
+    <ul>
+<li>[name]</li>
+<li>[name]-bts</li>
+<li>if you plan to use airtable automations to do extensive logging: [name]-notis</li>    
+</ul>  
+</p>
+
+<h3>Get featured in #happeings</h3>
+<p>Once you have a description for your ysws you should dm someone on the @newspapers team to get your ysws up on happenings!</p>
+<img src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/9fda627f4d9abdb43e24648d3eec1d2e8f048a66_image.png" />
+    <button className="btn btn-primary" onClick={() => nextPage()}>Next</button>
+    </div>
+    </div>
+    </div>
+}
--- src/pages/Website.jsx
+++ src/pages/Website.jsx
@@ -4,12 +4,11 @@ export default function app({ nextPage, backPage,currentPage }) {
     <div className="max-w-xl">
     <h1 className="text-5xl font-bold">2. Setup the website</h1>
     <p className="py-1">
-So to create the airtable at the moment you must fill out <a href="https://forms.hackclub.com/t/4SCbYrvsfgus" target="_blank">this form</a> and then wait for the organizer to create the base for you.
+To start with your ysws you want to have some sort of demo right? something which tells people who are doing your ysws what to <em>actually do!</em>
     </p>
-    <p>This form helps your processes in getting the stuff you need done asap., with this done you should be able to proceed on your own!</p>
+    <p>This website can be a simple html/css/js site or anything which can deploy on vercel! some examples are the <a href="https://github.com/hackclub/hacklet">Hacklet (GH)</a> and <a href="https://github.com/hackclub/grub">Grub (GH)</a>  source code!</p>
     <br />
-    <p>Here is what an example submission would look like:</p>
-    <img src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/dcd16cda829b3a132d5dd32ea6c57f97fd9d6ea3_image.png" alt="example" />
+    
     <button className="btn btn-primary" onClick={() => nextPage()}>Next</button>
     </div>
     </div>
